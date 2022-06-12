faaaa047cbc18e926b3877c1a5c7cb1f1c04db55
Javid I. Gafarli <javidig@code.local>
2022-06-12 06:18:45 -0700

    Homework 3

---
+++ Task/Task/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///////////////////////////////////////1/ Upper ///////////////////////////////////
+
+
+            //char[] BigLetters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            //char[] LilLetters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            //while (true)
+            //{
+
+
+
+            //    Console.WriteLine("Metn Daxil Edin : ");
+            //    string requiredText = Console.ReadLine();
+            //    string result = GetBigLetters(BigLetters, LilLetters, requiredText);
+            //    Console.WriteLine("Cevrilmish Metn : " + result);
+
+            //    static string GetBigLetters(char[] BigLetters, char[] LilLetters, string requiredText)
+            //    {
+
+            //        for (int i = 0; i < BigLetters.Length; i++)
+            //        {
+            //            requiredText = requiredText.Replace(LilLetters[i], BigLetters[i]);
+            //        }
+            //        return requiredText;
+            //    }
+
+
+            ////////////////////////////////////////2/ Lower ///////////////////////////////////////////
+
+
+
+
+
+
+
+
+            //    Console.WriteLine("Metn Daxil Edin : ");
+            //    string requiredText1 = Console.ReadLine();
+            //    string result1 = GetLilLetters(BigLetters, LilLetters, requiredText);
+            //    Console.WriteLine("Cevrilmish Metn : " + result1);
+
+            //    static string GetLilLetters(char[] BigLetters, char[] LilLetters, string requiredText1)
+            //    {
+
+            //        for (int i = 0; i < LilLetters.Length; i++)
+            //        {
+            //            requiredText1 = requiredText1.Replace(BigLetters[i], LilLetters[i]);
+            //        }
+            //        return requiredText1;
+            //    }
+            //}
+
+/////////////////////////////////////////////////3/ Letter ////////////////////////////////////////////
+
+            //Console.WriteLine("Hedef text : ");
+            //string requiredText = Console.ReadLine();
+            //Console.WriteLine("Istenilen character : ");
+            //char character = Convert.ToChar(Console.ReadLine());
+            //Console.WriteLine(isLetterHave( requiredText, character));
+
+            //static string isLetterHave(string requiredText, char character);
+            
+            //while (true)
+            //    {
+
+            //        for (int i = 0; i < requiredText.Length; i++)
+            //        {
+            //        if (requiredText[i] == character)
+            //            {
+            //                return "yes";
+            //            }
+
+            //        }
+            //        return "no";
+            //    }
+            
+        }
+    }
+}
+
---
+++ asf saafsd/asf saafsd/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace asf_saafsd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hedef text : ");
+            string requiredText = Console.ReadLine();
+            Console.WriteLine("Istenilen character : ");
+            char character = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(isLetterHave(requiredText, character));
+
+            static string isLetterHave(string requiredText, char character);
+            {
+                for (int i = 0; i < requiredText.Length; i++)
+                {
+                    if (requiredText[i] == character)
+                    {
+                        return "he";
+                    }
+
+                }
+                return "yox";
+            }
+        }
+    }
+}
